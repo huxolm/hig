fb7d2ed6536f5ee22e1da137f1568a82a401a081
Gary Sun <gary.sun@autodesk.com>
2019-07-12 15:27:25 +0800

    test: update jest tests

--- packages/side-nav/src/presenters/ExternalLinkIcon.test.js
+++ packages/side-nav/src/presenters/ExternalLinkIcon.test.js
@@ -1,17 +1,12 @@
 import { shallow } from "enzyme";
 import React from "react";
-import { ExternalLink16, ExternalLink24, sizes as iconSizes } from "@hig/icons";
+import { ExternalLinkUI } from "@hig/icons";
 
 import ExternalLinkIcon from "./ExternalLinkIcon";
 
 describe("side-nav/presenters/ExternalLinkIcon", () => {
-  it("defaults to the External Link 24px icon", () => {
+  it("default", () => {
     const wrapper = shallow(<ExternalLinkIcon />);
-    expect(wrapper.type()).toEqual(ExternalLink24);
-  });
-
-  it("allows size to be overridden", () => {
-    const wrapper = shallow(<ExternalLinkIcon size={iconSizes.PX_16} />);
-    expect(wrapper.type()).toEqual(ExternalLink16);
+    expect(wrapper.type()).toEqual(ExternalLinkUI);
   });
 });
